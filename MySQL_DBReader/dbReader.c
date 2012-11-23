1299562193bd4e9887a48e74ee63f80ad1f91d41
Christopher Morse <dignanmorse@gmail.com>
2012-11-22 23:46:04 -0500

    DBWriter renamed
    - .gitignore addition of DS_Store
    - Connection initialization and deallocation methods implemented in dbReader.
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- MySQL_DBReader/dbReader.c
+++ MySQL_DBReader/dbReader.c
@@ -1,9 +1,9 @@
 //
-//  main.c
-//  Snow
+//  dbReader.c
+//  SnowCloud
 //
 //  Created by Christopher Morse on 10/31/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Morse-Code. All rights reserved.
 //
 
 
@@ -13,6 +13,23 @@
 #include <errmsg.h>
 #include <string.h>
 
+
+int mysql_connection(MYSQL *conn) {
+
+    if (conn == NULL) {
+        printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
+    // Verify connection to propper database. 
+    if (mysql_real_connect(conn, "localhost", "chris", "feb2879", "snowdb", 0, NULL, 0) == NULL) {
+        printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+    
+    return 0;
+}
+
 int main(int argc, const char *argv[]) {
     MYSQL *conn;
     conn = mysql_init(NULL);
@@ -20,4 +37,10 @@ int main(int argc, const char *argv[]) {
     if (mysql_connection(conn) == 1) {
         exit(1);
     }
+
+
+
+    mysql_close(conn);
+
+    exit(0);
 }
