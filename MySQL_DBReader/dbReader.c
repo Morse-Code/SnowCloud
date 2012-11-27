cda1382bbcdb3e63ad2cc2438f1479312644876f
Christopher Morse <dignanmorse@gmail.com>
2012-11-27 13:36:28 -0500

    Working dbWriter and dbReader
    - Read from stdin and print to stdout.
    - Two Tables used: SNOWDATA and LATESTQUERY
    - LATESTQUERY maintains the index (primary key) of the last record retrieved.
    - This index is used during subsequent record retrievals to select all records added later than the index.
    - SNOWDATA uses an auto incrementing column as a Primary Key.
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- MySQL_DBReader/dbReader.c
+++ MySQL_DBReader/dbReader.c
@@ -3,7 +3,7 @@
 //  SnowCloud
 //
 //  Created by Christopher Morse on 10/31/12.
-//  Copyright (c) 2012 Morse-Code. All rights reserved.
+//  Copyright(c) 2012 Morse-Code. All rights reserved.
 //
 
 
@@ -14,6 +14,13 @@
 #include <string.h>
 
 
+int dbread(MYSQL *conn);
+
+/**************************************************************************************************
+/ Establish connection to MySQL database.
+/ Parameters: MySQL *conn
+/ Return: int 0 if successful; int 1 if error.
+**************************************************************************************************/
 int mysql_connection(MYSQL *conn) {
 
     if (conn == NULL) {
@@ -26,8 +33,63 @@ int mysql_connection(MYSQL *conn) {
         printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
         return 1;
     }
-    
     return 0;
+}
+
+/*  
+    int getLastEntry(MYSQL *conn)
+    {
+        int lastEntry;
+        MYSQL_RES *result;
+        MYSQL_ROW row;
+        char *query = "SELECT MAX(myindex) FROM LATESTQUERY";
+        mysql_query(conn, query);
+        row  = mysql_fetch_row(result);
+        lastEntry = row[0];
+        return lastEntry;
+    }
+*/
+
+/**************************************************************************************************
+/ Retrieve entries from database starting with the entry immediately following the most recent entry retrieved during the previeous session.
+/ Parameters: MYSQL *conn
+/ Return: int 0 if successful; int <> 0 if error.
+**************************************************************************************************/
+int dbread(MYSQL *conn){
+    MYSQL_RES *result;
+    MYSQL_ROW row;
+    int i;
+    char *query;
+
+    // int lastEntry = getLastEntry(conn);
+    mysql_query(conn,"SELECT mote, cattribute, dattribute, SNOWDATA.epoch, mytime, myindex "
+                     "FROM SNOWDATA "
+                     "WHERE SNOWDATA.myindex > (SELECT MAX(LATESTQUERY.myindex) "
+                                               "FROM LATESTQUERY) ORDER BY SNOWDATA.myindex"
+                );
+    result = mysql_store_result(conn);
+
+    int num_fields = mysql_num_fields(result);
+
+    while ((row = mysql_fetch_row(result)))
+    {     
+        for(i = 0; i < (num_fields - 2); i++)
+        {
+            printf("%s\t", row[i] ? row[i] : "NULL");
+        }
+        printf("\n");
+        char *entry = "INSERT INTO LATESTQUERY(myindex) VALUES('%s')";
+        asprintf(&query, entry, row[5]);
+        /*free(query);
+        free(entry);*/
+    }
+    if(mysql_query(conn, query) != 0){
+        printf("SQL Error: %d, %s%c", mysql_errno(conn), mysql_error(conn), '\n');
+    }
+
+
+  mysql_free_result(result);
+  return 0;
 }
 
 int main(int argc, const char *argv[]) {
@@ -39,9 +101,8 @@ int main(int argc, const char *argv[]) {
     }
 
 
-
+    dbread(conn);
     mysql_close(conn);
 
     exit(0);
 }
-
---
+++ MySQL_DBWriter/dbWriter.c
@@ -0,0 +1,125 @@
+//
+//  dbWriter.c
+//  SnowCloud
+//
+//  Created by Christopher Morse on 10/31/12.
+//  Copyright (c) 2012 Morse-Code. All rights reserved.
+//
+
+
+#include <my_global.h>
+#include <mysql.h>
+#include <ctype.h>
+#include <errmsg.h>
+#include <string.h>
+
+
+int dbwrite(MYSQL *conn, char *mote, char *cattribute, char *dattribute, char *epoch);
+void parsetokens(MYSQL *conn);
+
+/**********************************************************************************************
+/ Establish connection to MySQL database. Table creation code is commented. Uncomment to initialize new tables..
+/ Parameters: MySQL *conn
+/ Return: int 0 if successful; int 1 if error.
+**********************************************************************************************/
+int mysql_connection(MYSQL *conn) {
+
+    if (conn == NULL) {
+        printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+
+    // Verify connection to propper database. 
+    if (mysql_real_connect(conn, "localhost", "chris", "feb2879", "snowdb", 0, NULL, 0) == NULL) {
+        printf("Error %u: %s\n", mysql_errno(conn), mysql_error(conn));
+        return 1;
+    }
+    
+    // Create tables. Really not necessary after the tables have been initialized. Comment out for now.
+    //mysql_query(conn, "CREATE TABLE SNOWDATA(mote int not null, cattribute int not null, dattribute int not null, epoch int not null, mytime int NOT NULL, myindex int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY)");
+    // mysql_query(conn, "CREATE TABLE TIMEKEEPER(time int NOT NULL, epoch int NOT NULL, myindex int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY)");
+    //mysql_query(conn, "CREATE TABLE LATESTQUERY(myindex int unsigned NOT NULL)");
+
+    return 0;
+}
+
+/**************************************************************************************************
+/ Parses incoming data from stdin into tokens. Tokens are fed as arguments to dbwrite().
+/ Parameters: MYSQL connection, conn.
+**************************************************************************************************/
+void parsetokens(MYSQL *conn){
+    char *dattribute, *epoch, *mote, *cattribute;
+    const char delimiters[] = "mcdr";
+    size_t nbytes = 100;
+    char *line = (char *) malloc(nbytes + 1);
+    int bytes_read;
+    bytes_read = getline(&line, &nbytes, stdin);
+    while (bytes_read != -1){
+        mote = strtok(line, delimiters);
+        cattribute = strtok(NULL, delimiters);
+        dattribute = strtok(NULL, delimiters);
+        epoch = strtok(NULL, delimiters);
+        int success = dbwrite(conn, mote, cattribute, dattribute, epoch);
+        printf("Characters printed: %d\n", success);
+        bytes_read = getline(&line, &nbytes, stdin);
+    }
+}
+
+/**************************************************************************************************
+/ Write single row tuple to database and corresponding timestamp -epoch tuple to timekeeper db.
+/ Parameters: MySQL *conn, char *mote, char *cattribute, char *dattribute, char *epoch
+/ Return: int result; The number of characters written to the database.
+**************************************************************************************************/
+int dbwrite(MYSQL *conn, char *mote, char *cattribute, char *dattribute, char *epoch) {
+    int success;
+
+    if (epoch[0] != '1'){
+        return 0;
+    }else{
+        printf("%s",epoch);
+        epoch = &epoch[1];
+        printf("%c",'\n');
+        printf("%s",epoch);
+
+    }
+/*  
+    char *timedata = "INSERT INTO TIMEKEEPER(time, epoch, myindex) VALUES('%d', '%s', NULL)";
+    char *timequery;
+    int tstamp = (int)time(NULL);
+    asprintf(&timequery, timedata, tstamp, epoch);
+    int success = mysql_query(conn, timequery);
+    if (success != 0) {
+        printf("SQL Error: %d, %s%c", mysql_errno(conn), mysql_error(conn), '\n');
+    }
+*/
+    char *stat = "INSERT INTO SNOWDATA(mote, cattribute, dattribute, epoch, mytime, myindex) VALUES('%s', '%s', '%s', '%s', '%d', NULL)";
+    char *query;
+    int tstamp = (int)time(NULL);
+    asprintf(&query, stat, mote, cattribute, dattribute, epoch, tstamp);
+    success = mysql_query(conn, query);
+    if (success != 0) {
+        printf("SQL Error: %d, %s%c", mysql_errno(conn), mysql_error(conn), '\n');
+    }
+    int result = printf("%s%c", query, '\n');
+
+    fputc('\n', stdout);
+    free(query);
+    return result;
+
+}
+
+int main(int argc, const char *argv[]) {
+    MYSQL *conn;
+    conn = mysql_init(NULL);
+
+    if (mysql_connection(conn) == 1) {
+        exit(1);
+    }
+
+    //TODO Function to test existence of Tables. If found, return success, else create tables. 
+
+    parsetokens(conn);
+    mysql_close(conn);
+
+    exit(0);
+}
