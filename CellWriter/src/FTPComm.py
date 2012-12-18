460ff2dcf3e415f4dab37d88faa7efa2277ea0cb
Christopher Morse <dignanmorse@gmail.com>
2012-12-18 03:15:24 -0500

    Class ATCommands encapsulates AT Command set  - working over USB serial connection using pySerial module.  - Connection and modem communication working  - started regex generation for parsing responses  - initial problems with the format of response data (line endings are  '\r\n')  - TODO need to expand handling of response strings and values to adjust  behavior appropriately.  - regex will be more concise and reliable than string operation (ie split()  , strip())  - compile regex objects for match and split operations  - perhaps create custom class or set of methods for handling response data.  - continue to modularize code.

--- CellWriter/src/FTPComm.py
+++ CellWriter/src/FTPComm.py
@@ -1,30 +1,70 @@
 # FTPComm
 # 12/15/12
-import sys
-import io
 
 __author__ = 'Christopher Morse'
 
+import time
+
 import serial
-
-serialPort = serial.Serial(port='/dev/tty.Bluetooth-PDA-Sync',
-                           baudrate=115200,
-                           timeout=0, rtscts=True,
-                           xonxoff=False)
+import re
 
 
-def sendatcmd(cmd):
-    serialPort.write('at' + cmd + '\r')
+class ATCommands:
+    """"""
 
+    def __init__(self):
+        """Constructor for """
+        self.newLine = ('\r\n')
 
-print 'Loading profile...',
-sendatcmd('+npsda=0,2')
+    def initModem(self, port='/dev/tty.usbserial-A9014MJT',
+                  baudrate=115200,
+                  timeout=1):
+        self.ser = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+        while (self.sendCommand('') != 'OK'):
+            time.sleep(.5)
+        time.sleep(1)
+        while (self.sendCommand('E0') != 'OK'):
+            time.sleep(.5)
+        return 1
+
+    def checkSignal(self):
+        command = '+CSQ'
+        s = re.compile('\s*(.*:)*\s*(?P<signal>[0-9]+),.*')
+        response = self.sendCommand(command)
+        m = s.match(response)
+        signal = m.group('signal')
+        print(signal)
+        return signal
+
+    def sendCommand(self, command, getline=True):
+        self.ser.write('AT' + command + '\r')
+        data = ''
+        if getline:
+            data = self.ReadLine()
+        return data
+
+    def ReadLine(self):
+        data = self.ser.read(256)
+        print(data.split())
+        data = data.strip()
+        r = re.compile("\s*((?P<type>.+):)*\s*(?P<value>.+),*.*")
+        m = r.match(data)
+        responseType = m.group('type')
+        responseValue = m.group('value')
+        print(data)
+        self.ser.flush()
+        return data
+
+    def closeConn(self):
+        self.ser.close()
 
 
 def main():
-    sendatcmd("cmd")
-    sendatcmd("hello")
-    print("hello")
+    modem = ATCommands()
+    modem.initModem()
+    print("Thats all folks")
+    time.sleep(.5)
+    modem.checkSignal()
 
 
 if __name__ == "__main__":
